59f30d605ac6a0ac6c757008be8e0535bd4478a8
Hasan Cobanoglu <hasancobanoglu@Hasans-MacBook-Pro.local>
2023-09-12 14:53:19 -0500

    create model

---
+++ CarListingAppDemoMaui/CarListingAppDemoMaui/Model/Car.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace CarListingAppDemoMaui.Model
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Vin { get; set; }
+    }
+}
+
---
+++ CarListingAppDemoMaui/CarListingAppDemoMaui/Repository/CarRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using CarListingAppDemoMaui.Model;
+
+namespace CarListingAppDemoMaui.Service
+{
+    public class CarRepository
+    {
+        public List<Car> GetCars()
+        {
+            return new List<Car>()
+            {
+                new Car
+                {
+                    Id = 1,
+                    Make = "Honda",
+                    Model = "Fit",
+                    Vin ="123",
+                },
+                new Car
+                {
+                    Id = 2,
+                    Make = "Toyota",
+                    Model = "Prado",
+                    Vin ="1234",
+                },
+                new Car
+                {
+                    Id = 3,
+                    Make = "Honda",
+                    Model = "Civic",
+                    Vin ="123",
+                },
+                 new Car
+                {
+                    Id = 4,
+                    Make = "BMW",
+                    Model = "M3",
+                    Vin ="123e",
+                },
+                  new Car
+                {
+                    Id = 5,
+                    Make = "Ferrari",
+                    Model = "Spider",
+                    Vin ="123s",
+                },
+            };
+        }
+    }
+}
+
